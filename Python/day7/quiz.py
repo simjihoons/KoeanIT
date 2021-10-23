7f4b25844e7cf98437a06dec8af2956bd1a2eac3
unknown <wlgnsgit@gmail.com>
2021-10-23 17:51:23 +0900

    2021-10-23

--- Python/day7/quiz.py
+++ Python/day7/quiz.py
@@ -180,3 +180,25 @@ class Car():
 
 class Bycicle(Car):
     pass
+
+
+#%%
+# 자동차 클래스
+# 바퀴갯수 , 출고가격 인스턴스변수로 갖는다.
+# car = Car(4,2000)
+
+
+class Car():
+    def __init__(self, wheel, price) -> None:
+        self.wheel = wheel
+        self.price = price
+
+
+class Bicycle(Car):
+    def __init__(self, wheel, price):
+        super().__init__(wheel, price)
+
+
+bicycle = Bicycle(2, 100)
+print(bicycle.wheel)
+print(bicycle.price)
