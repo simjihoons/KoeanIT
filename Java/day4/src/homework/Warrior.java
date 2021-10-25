42472c7350092b4191f0190573d3028a2a5764d2
jihoonsim <wlgnsgit@gmail.com>
2021-10-25 17:43:31 +0900

    2021-10-25

---
+++ Java/day4/src/homework/Warrior.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class Warrior {
+//멤버 변수]
+//hp ,power,name 등
+	//다 get set으로 하기
+}
---
+++ Java/day4/src/homework/Wizard.java
@@ -0,0 +1,7 @@
+package homework;
+
+public class Wizard {
+	//멤버 변수]
+	//hp ,power,name 등
+		//다 get set으로 하기
+}
