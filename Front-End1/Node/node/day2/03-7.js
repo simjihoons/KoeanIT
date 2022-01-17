ea8b06864a41eaebb982bbd1f9881e5f0fba4a97
simjihoons <wlgnsgit@gmail.com>
2022-01-17 12:52:22 +0900

    22.01.17 익스프레스 3

---
+++ Front-End1/Node/node/day2/03-7.js
@@ -0,0 +1,44 @@
+/*
+    익스프레스 모듈
+   http 모듈만 사용해서 웹 서버를 구성하면 직접 많은 기능을 개발해야한다
+   이 문제를 해결하기 위해 만들어진 모듈이 익스프레스이다.
+   익스프레스 모듈을 사용하면 간단한 코드로 웹 서버의 기능을 대부분 구현할 수 있고, 미들웨어와
+   라우터를 사용하여 편리하게 웹 서버를 구성가능
+   따로 설치를 해야 한다.
+
+    [package 설치]
+    npm install 모듈명(npm i 모듈명)
+    npm i express
+
+    [package 복원]
+    npm install
+
+
+    메소드
+    use() :
+    get() :
+    set() :
+    redirect() :
+    send() :
+    header() :
+*/
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res) => {
+  console.log("첫번쨰 미들웨어 실행");
+
+  console.dir(req.header);
+  const userAgent = req.header("User-Agent");
+  console.log(userAgent);
+
+  //http://localhost:3000/?userid=apple
+  const paramName = req.query.userid; //get방식의 변수값을 가져옴
+  console.log(paramName);
+});
+
+app.listen(port, () => {
+  console.log(`${port}포트로 실행`);
+});
