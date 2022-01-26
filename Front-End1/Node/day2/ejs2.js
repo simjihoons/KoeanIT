c458845064f3c1789f91f09c4b189d535fb1ddaa
simjihoons <wlgnsgit@gmail.com>
2022-01-26 09:40:06 +0900

    22.01.26 ejs2

--- Front-End1/Node/day2/ejs2.js
+++ Front-End1/Node/day2/ejs2.js
@@ -5,16 +5,17 @@ const ejs = require("ejs"); //npm i ejs
 
 const app = express();
 const port = 3000;
-
 const router = express.Router();
 
-// localhost:3000/login
-router.route("/login").post((req, res) => {
-  const useinfo = { userid: "apple", userpw: "1234" };
-  fs.readFile("./ejs2.ejs", "utf8", (err, data) => {
+// localhost:3000/ejstest
+router.route("/ejslogin").post((req, res) => {
+  const userinfo = { userid: "apple", userpw: "1234" };
+  //매개변수를 발생해서 직접 ejs 파일에 변수 입력
+
+  fs.readFile("./ejs2.ejs", "utf-8", (err, data) => {
     if (!err) {
       res.writeHead(200, { "content-type": "text/html" });
-      res.end(ejs.render(data, useinfo));
+      res.end(ejs.render(data));
     } else {
       console.log(err);
     }
@@ -22,11 +23,9 @@ router.route("/login").post((req, res) => {
 });
 
 app.use("/", router);
-
 app.all("*", (req, res) => {
   res.status(404).send("<h2>페이지를 찾을 수 없습니다.</h2>");
 });
-
 app.listen(port, () => {
   console.log(`${port} 포트로 서버 실행중...`);
 });
