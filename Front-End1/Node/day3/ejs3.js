9697aefb8cf1c4a715c2d1214e1f39642870a308
simjihoons <wlgnsgit@gmail.com>
2022-01-26 10:48:15 +0900

    22.01.26 ejs3

---
+++ Front-End1/Node/day3/ejs3.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const app = express();
+const port = 3000;
+
+const router = express.Router();
+
+//여러개의 ejs 파일을 각각 불러오는 방법
+const header = fs.readFileSync("./ejs header.ejs", "utf8");
+const body = fs.readFileSync("./ejs body.ejs", "utf8");
+
+// localhost:3000/about
+router.route("/about").post((req, res) => {
+  const html = ejs.render(header, {
+    title: "매개변수로 전달된 제목입니다.",
+    content: ejs.render(body, {
+      message: "매개변수로 전달된 텍스트 메세지입니다.",
+    }),
+  });
+  res.writeHead(200, { "content-type": "text/html" });
+  res.end(html);
+});
+
+app.use("/", router);
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h2>페이지를 찾을 수 없습니다.</h2>");
+});
+
+app.listen(port, () => {
+  console.log(`${port} 포트로 서버 실행중...`);
+});
