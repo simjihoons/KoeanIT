bf26967b27674d00230c8ffeb2f81a7451faa4b3
simjihoons <wlgnsgit@gmail.com>
2022-02-07 10:19:16 +0900

    node 기본정리

--- Front-End1/Node/day4/router/module1.js
+++ Front-End1/Node/day4/router/module1.js
@@ -1,8 +1,13 @@
-// const express = require("express");
-// const app = express();
+//npm i ejs
 
 module.exports = (app) => {
   app.get("/", (req, res) => {
     console.log("module1.js 실행");
+
+    res.render("index.ejs", { length: 10 }); // ejs파일에 변수값을 전달
+  });
+
+  app.get("/about", (req, res) => {
+    res.render("about.html");
   });
 };
