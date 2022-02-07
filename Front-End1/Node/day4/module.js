97c9b16979d2cc6bf321d7b57e029c7d33946c5f
simjihoons <wlgnsgit@gmail.com>
2022-02-07 10:33:20 +0900

    22.02.07 사용자 지정 모듈

--- Front-End1/Node/day4/module.js
+++ Front-End1/Node/day4/module.js
@@ -12,5 +12,5 @@ app.use(bodyParser.urlencoded({ extended: false })); //post 방식
 const module1 = require("./router/module1")(app); //express()를 전달
 
 app.listen(port, () => {
-  console.log(`${port}번 포트로 서버 실행중...`);
+  console.log(`${port}번 포트로 서버 실행중....`);
 });
--- Front-End1/Node/day4/router/module1.js
+++ Front-End1/Node/day4/router/module1.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   app.get("/", (req, res) => {
     console.log("module1.js 실행");
 
-    res.render("index.ejs", { length: 10 }); // ejs파일에 변수값을 전달
+    res.render("index.ejs", { length: 10 }); // ejs파일에 변수값을 전달.
   });
 
   app.get("/about", (req, res) => {
