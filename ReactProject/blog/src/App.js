85c0d5390fbfca6553e05dbac1237d4ce97cebc5
simjihoons <wlgnsgit@gmail.com>
2022-02-22 17:12:28 +0900

    Apple React

--- ReactProject/blog/src/App.js
+++ ReactProject/blog/src/App.js
@@ -12,6 +12,8 @@ function App() {
     "파리바게뜨",
   ]);
 
+  let [누른제목, 누른제목변경] = useState(0);
+
   let [따봉, 따봉변경] = useState(0);
 
   let posts = "강남 고기 맛집";
@@ -23,6 +25,10 @@ function App() {
   //   글제목변경(newArray);
   // }
 
+  let [modal, modal변경] = useState(false);
+
+  let [입력값, 입력값변경] = useState("");
+
   return (
     <div className="App">
       <div className="black-nav">
@@ -31,34 +37,40 @@ function App() {
 
       <div className="list">
         <h4>
-          {글제목[0]} <span className="btn1" onClick={()=>{
-            따봉변경(따봉+1);
-          }}>👍</span>
+          {글제목[0]}{" "}
+          <span
+            className="btn1"
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>
           {따봉}
         </h4>
         <p>2월 17일 발행</p>
         <hr />
       </div>
 
-      <div className="list">
-        <h1>{글제목[1]}</h1>
-        <p>3월 17일 발행</p>
-        <hr />
-      </div>
+      {
+        //1. for문 대신 쓸수 있는 map()
+        글제목.map(function (title, i) {
+          return (
+            <div className="list" key={i}>
+              <h1
+                onClick={() => {
+                  누른제목변경(i);
+                }}
+              >
+                {title}
+              </h1>
+              <p>4월 17일 발행</p>
+              <hr />
+            </div>
+          );
+        })
+      }
 
-      <div className="list">
-        <h1>{글제목[2]}</h1>
-        <p>3월 17일 발행</p>
-        <hr />
-      </div>
-
-      <div className="list">
-        <h1>{글제목[3]}</h1>
-        <p>4월 17일 발행</p>
-        <hr />
-      </div>
-
-   
       {/* -----------------------------------------------------------------
         <div className="modal">
         <h2>제목</h2>
@@ -69,26 +81,61 @@ function App() {
         아래와 같은 코드(컴포넌트 이용)
         Modal 함수 활용
         */}
-        
-        <Modal />
-          
-        
+      {/* 
+      {입력값}
+      <input
+        onChange={(e) => {
+          // console.log(e.target.value);
+          입력값변경(e.target.value);
+        }}
+      /> */}
+
+      <div className="publish">
+        <input
+          onChange={(e) => {
+            입력값변경(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            var array = [...글제목];
+            array.unshift(입력값); //unshift => array 맨앞에 자료 추가하는 문법
+            글제목변경(array);
+          }}
+        >
+          저장
+        </button>
+      </div>
+
+      <button
+        onClick={() => {
+          modal변경(!modal);
+        }}
+      >
+        열고닫기
+      </button>
+
+      {
+        //{} 안에 if는 못쓰기 때문에 삼항연산자 이용
+
+        modal === true ? <Modal 글제목={글제목} 누른제목={누른제목} /> : null
+      }
+
       {/* ------------------------------------------------------------ */}
     </div> //end of App
   );
 }
- 
-function Modal() {
+
+function Modal(props) {
   return (
     <>
-    <div className="modal">
-      <h2>제목</h2>
-      <p>날짜</p>
-      <p>상세내용</p>
-    </div>
-  </>
+      <div className="modal">
+        <h2>{props.글제목[props.누른제목]}</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
   );
 }
-
 
 export default App;
