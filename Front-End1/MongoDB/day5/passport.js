3db03cafc70d8602fb120bcb446bb9d8f0d3b3f7
simjihoons <wlgnsgit@gmail.com>
2022-02-23 10:47:47 +0900

    22.02.23 passport + facebook Login

---
+++ Front-End1/MongoDB/day5/passport.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const static = require("serve-static");
+const path = require("path");
