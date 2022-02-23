a1fd09885e8d7ba809df3be38b543019e2ab8771
simjihoons <wlgnsgit@gmail.com>
2022-02-23 10:47:59 +0900

    22.02.23 passport + facebook Login

--- Front-End1/MongoDB/day5/passport.js
+++ Front-End1/MongoDB/day5/passport.js
@@ -3,5 +3,5 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const expressSession = require("express-session");
-const static = require("serve-static");
-const path = require("path");
+const static = require("serve-static"); //페이지에 직접 접근 해주는 모듈
+const path = require("path"); //url 접근을 해야 하므로 패스 모듈 설치
