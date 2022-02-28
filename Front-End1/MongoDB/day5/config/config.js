748514cbe7b894f79ae0bbf6e6d1d48820ee0ba2
simjihoons <wlgnsgit@gmail.com>
2022-02-28 13:33:28 +0900

    회원가입 관련

--- Front-End1/MongoDB/day5/config/config.js
+++ Front-End1/MongoDB/day5/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   server_port: 3000, //port 설정
-  db_url: "mongodb://localhost:27017/frontenddb", // DB 연결
+  db_url: "mongodb://localhost:27017/frontendb01", // DB 연결
 
   //스키마 생성
   db_schemas: [
@@ -14,8 +14,8 @@ module.exports = {
 
   //페이스북에서 가져올 세션
   facebook: {
-    clientID: "",
-    clientSecret: "",
+    clientID: "532728584733522",
+    clientSecret: "67758765c0390e3d431d5abe2cf51afa",
     callbackURL: "http://localhost:3000/auth/facebook/callback",
   },
 };
