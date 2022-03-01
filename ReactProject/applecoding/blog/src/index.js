0d8799207caae0d4e855d2c5a7226e58f415b66f
simjihoons <wlgnsgit@gmail.com>
2022-03-01 13:58:14 +0900

    appleCoding

--- ReactProject/applecoding/blog/src/index.js
+++ ReactProject/applecoding/blog/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App2 from "./App";
+import App3 from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 />
+    <App3 />
   </React.StrictMode>,
   document.getElementById("root")
 );
