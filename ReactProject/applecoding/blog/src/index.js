82b0f9829cb675e52cd0bc03b605273ebf3dab3c
simjihoons <wlgnsgit@gmail.com>
2022-03-02 14:42:27 +0900

    appleCoding

--- ReactProject/applecoding/blog/src/index.js
+++ ReactProject/applecoding/blog/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App6";
+import App from "./App9";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
@@ -15,12 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// 데이터 바인딩
-
-// state문법
-// 	1. 변수 대신 쓰는 데이터 저장 공간
-// 	2. usestate()를 이용해서 만들어야함
-// 	=> state에 데이터 저장해놓는 이유 : 웹이 App처럼 동작하게 만들고 싶어서 , state는 변경되면 HTML이 자동으로 재렌더링이 되기 때문(새로고침 없이도)
-
-// 	리액트에선 UI를 만들 때 state 데이터를 이용한다.
