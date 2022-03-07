27c34c3597844bf1e6ca911b013b75e6161978ea
simjihoons <wlgnsgit@gmail.com>
2022-03-07 20:57:10 +0900

    Ajax 요청방법

--- ReactProject/applecoding/shop/src/index.js
+++ ReactProject/applecoding/shop/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App8";
+import App from "./App10";
 
 import { BrowserRouter } from "react-router-dom";
 
