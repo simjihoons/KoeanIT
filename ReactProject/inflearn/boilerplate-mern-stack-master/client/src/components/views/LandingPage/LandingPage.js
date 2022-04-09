066bb17e2e5d3731a6aac34a14c4d08de6e25f32
simjihoons <wlgnsgit@gmail.com>
2022-04-09 23:36:35 +0900

    랜딩페이지 만들기 - 1. DB에 들어있는 모든 상품 가져오기

--- ReactProject/inflearn/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ ReactProject/inflearn/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { FaCode } from "react-icons/fa";
 
+// 백엔드에 요청을 보내기위해
+import axios from "axios";
+
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  useEffect(() => {
+    axios.post("/api/product/products").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+      } else {
+        alert("상품들을 가져오는데 실패");
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <div>LandingPage</div>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
