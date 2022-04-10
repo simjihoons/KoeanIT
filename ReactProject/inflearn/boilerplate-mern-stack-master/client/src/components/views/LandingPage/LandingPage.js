c2d19ec3f2c58f049043ed7935df9e990825be5d
simjihoons <wlgnsgit@gmail.com>
2022-04-10 21:11:57 +0900

    랜딩 페이지 만들기 5. 체크박스 필터 만들기

--- ReactProject/inflearn/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ ReactProject/inflearn/boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -11,29 +11,61 @@ import Meta from "antd/lib/card/Meta";
 //이미지 슬라이더
 import ImageSlider from "../../utils/ImageSlider";
 
+//체크박스
+import Checkbox from "./Sections/CheckBox";
+import { continents } from "./Sections/Datas";
+
 function LandingPage() {
   const [Products, setProducts] = useState([]);
   const [Skip, setSkip] = useState(0);
   const [Limit, setLimit] = useState(8); //화면에 8개만 보여주기
+
+  const [PostSize, setPostSize] = useState(0);
+
+  const [Filters, setFilters] = useState({
+    continents: [],
+    price: [],
+  });
 
   useEffect(() => {
     let body = {
       skip: Skip,
       limit: Limit,
     };
+    getProducts(body);
+  }, []);
 
+  const getProducts = (body) => {
     axios.post("/api/product/products", body).then((response) => {
       if (response.data.success) {
-        console.log(response.data);
-        setProducts(response.data.productInfo);
+        if (body.loadMore) {
+          //만약 더보기 버튼을 눌렀다면~
+          setProducts([...Products, ...response.data.productInfo]);
+        } else {
+          setProducts(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
       } else {
         alert("상품들을 가져오는데 실패");
       }
     });
-  }, []);
+  };
 
   const loadMoreHandler = () => {
     //   skip 과 limit은 state로 관리
+
+    //처음 더보기를 누르면 0 + 8
+    //두번째는 8 + 8
+    let skip = Skip + Limit;
+
+    let body = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+    };
+
+    getProducts(body);
+    setSkip(skip);
   };
 
   const renderCards = Products.map((product, index) => {
@@ -49,6 +81,25 @@ function LandingPage() {
     );
   });
 
+  const showFilteredResults = (filters) => {
+    let body = {
+      skip: 0, //체크박스를 여러개 누를때마다 다시 시작해야하기 때문
+      limit: Limit,
+      filters: filters,
+    };
+
+    getProducts(body);
+    setSkip(0);
+  };
+
+  const handleFilters = (filters, category) => {
+    const newFilters = { ...Filters };
+
+    newFilters[category] = filters;
+
+    showFilteredResults(newFilters);
+  };
+
   return (
     <>
       <div style={{ width: "75%", margin: "3rem auto" }}>
@@ -58,17 +109,25 @@ function LandingPage() {
           </h2>
         </div>
 
-        {/* Filter */}
-
+        {/* Filter start=============*/}
+        {/* checkBox */}
+        <Checkbox
+          list={continents}
+          handleFilters={(filters) => handleFilters(filters, "continents")}
+        />
+        {/* radioBox */}
+        {/* Filter end=============*/}
         {/* Search */}
 
         {/* Card */}
         {/* gutter =>이미지들  사이에 여백을  주기위해 */}
         <Row gutter={[16, 16]}>{renderCards}</Row>
 
-        <div style={{ justifyContent: "center" }}>
-          <button onClick={loadMoreHandler}>더보기</button>
-        </div>
+        {PostSize >= Limit && (
+          <div style={{ justifyContent: "center" }}>
+            <button onClick={loadMoreHandler}>더보기</button>
+          </div>
+        )}
       </div>
     </>
   );
