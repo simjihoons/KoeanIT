3490901314adc0ce98edfa9b080b19d77afcc4d0
simjihoons <wlgnsgit@gmail.com>
2022-04-18 22:11:19 +0900

    Redux , Reducer

---
+++ ReactProject/boiler-plate/client/src/_reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+// import user form "./user_reducer";
+
+//combineReducers => 여러 리듀서를 하나(rootReducer)로 합쳐줌
+const rootReducer = combineReducers({
+  //user
+});
+
+export default rootReducer;
--- ReactProject/boiler-plate/client/src/index.js
+++ ReactProject/boiler-plate/client/src/index.js
@@ -5,9 +5,28 @@ import App from "./App";
 
 import "antd/dist/antd.css";
 
+//리덕스연결
+import Provider from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
 root.render(
-  <React.StrictMode>
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
-  </React.StrictMode>
+  </Provider>
 );
